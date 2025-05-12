166ac3571a27101b0c16b4acfc457b9c92d25dbc
Belkiz2003 <belkizguelduer@gmail.com>
2025-05-12 12:27:58 +0200

    Methoden ins interface geschrieben

--- PI2_ULP3_Listen/src/IntegerList.java
+++ PI2_ULP3_Listen/src/IntegerList.java
@@ -1,3 +1,13 @@
 public interface IntegerList {
-    
+    int getLength();
+
+    int insertLast(int value);
+
+    int getFrist();
+
+    int deleteFirst();
+
+    boolean search(int value);
+
+    void print();
 }
