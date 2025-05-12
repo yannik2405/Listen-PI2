3b048a9f92a90ba883aa4e190fb5ddd08d08c857
Belkiz2003 <belkizguelduer@gmail.com>
2025-05-12 13:31:45 +0200

    Aufgabe 3.1 fertig

--- PI2_ULP3_Listen/src/ArrayIntegerList.java
+++ PI2_ULP3_Listen/src/ArrayIntegerList.java
@@ -1,2 +1,62 @@
-public class ArrayIntegerList {
+public class ArrayIntegerList implements IntegerList {
+    private int liste[];
+    private int length = 0;
+    ArrayIntegerList(int max){
+        liste = new int[max];
+    }
+    public int getLength(){
+        return length;
+    }
+
+    public int insertLast(int value){
+
+        if(length > liste.length){
+            return -1;
+        }
+        liste[length] = value;
+        length ++;
+        return 0;
+    }
+
+
+    public int getFrist() {
+        if (length == 0)
+        {
+            return -9999;
+        }
+        return liste[0];
+    }
+
+    public int deleteFirst(){
+        if(length == 0)
+        {
+            return -1;
+        }
+
+        for(int i = 0; i < length -1; i++)
+        {
+             liste[i] = liste[i +1];
+        }
+        length--;
+        return 0;
+    }
+
+    public boolean search(int value){
+        for(int i =0;i < length; i++)
+        {
+            if(value == liste[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print(){
+        for(int i =0;i < length; i++)
+        {
+            System.out.print(liste[i]+", ");
+
+        }
+        System.out.println();
+    }
 }
--- PI2_ULP3_Listen/src/Main.java
+++ PI2_ULP3_Listen/src/Main.java
@@ -1,5 +1,95 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+        public static void main(String[] args) {
+            
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Bitte geben sie die gewünschte Größe der Liste ein!");
+            
+            int size = scanner.nextInt();
+
+            ArrayIntegerList list = new ArrayIntegerList(size);
+            
+            boolean laufend = true;
+
+            while (laufend) {
+
+                System.out.println("----Bitte wählen Sie einen Menü Punkt----");
+                System.out.println("1: getLenght()");
+                System.out.println("2: insertlast()");
+                System.out.println("3: getFirst()");
+                System.out.println("4: deleteFirst()");
+                System.out.println("5: search()");
+                System.out.println("6: print()");
+                System.out.println("7: Beenden");
+
+                int auswahl = scanner.nextInt();
+
+                switch (auswahl) {
+
+                    case 1:
+                        System.out.println("Aktuelle Länge der Liste: " + list.getLength());
+                        break;
+
+                    case 2:
+                        System.out.println("Geben Sie einen Wert ein, welcher eingefügt werden soll!");
+                        int eingabe = scanner.nextInt();
+                        if(list.insertLast(eingabe) == 0)
+                        {
+                            System.out.println("Der Wert wurde erfolgreich hinzugefügt!");
+                        }
+                        else {
+                            System.out.println("Der Wert konnte nicht hinzugefügt werden.");
+                        }
+                        break;
+
+                    case 3:
+                        if(list.getFrist() == -9999)
+                        {
+                            System.out.println("Die Liste ist leer.");
+                        }
+                        else {
+                            System.out.println("Das erste Element der Liste ist: " + list.getFrist());
+                        }
+                        break;
+
+                    case 4:
+                        if(list.deleteFirst() == 0)
+                        {
+                            System.out.println("Das erste Element wurde erfolgreich entfernt!");
+                        }
+                        else {
+                            System.out.println("Es konnte kein Element entfernt werden!");
+                        }
+                        break;
+
+                    case 5:
+                        System.out.println("Geben Sie einen Wert ein, welcher gesucht werden soll!");
+                        int suche = scanner.nextInt();
+                        if(list.search(suche))
+                        {
+                            System.out.println("Das Element ist in der Liste enthalten.");
+                        }
+                        else {
+                            System.out.println("Das Element ist NICHT in der Liste enthalten.");
+                        }
+                        break;
+
+                    case 6:
+                        list.print();
+                        break;
+
+                    case 7:
+                        laufend = false;
+                        System.out.println("Programm wird beendet.");
+                        break;
+
+                   
+                    default:
+                        System.out.println("Ungültige Eingabe. Bitte wählen Sie eine Option von 1 bis 5.");
+                }
+            }
+        }
     }
-}
